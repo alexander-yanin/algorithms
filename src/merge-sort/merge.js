ba436386c44ef833e56ab4875398efbe9ac1d0c6
Yanin Alexander <alexandr.nn54@gmail.com>
2019-06-16 21:49:10 +0700

    added merge function with testing

---
+++ src/merge-sort/merge.js
@@ -0,0 +1,28 @@
+/**
+ * @param left - Array of numbers
+ * @param right - Array of numbers
+ */
+
+function merge(left, right) {
+  const result = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift());
+    } else {
+      result.push(right.shift());
+    }
+  }
+
+  while (left.length) {
+    result.push(left.shift());
+  }
+
+  while (right.length) {
+    result.push(right.shift());
+  }
+
+  return result;
+}
+
+module.exports = merge;
